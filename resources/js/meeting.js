f08e0806242e00ce9e2bfcd09a8805043ed927a3
chajoonho <bsybsy2109@gmail.com>
2024-06-12 17:30:49 +0900

    .

---
+++ resources/js/meeting.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".meeting-slide", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
