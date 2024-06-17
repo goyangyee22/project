f1d496edfcbdb12f836de8ad3fe6080486fbcf8f
bsybsy01 <bsybsy2109@gmail.com>
2024-06-17 17:29:24 +0900

    .

--- resources/js/meeting.js
+++ resources/js/meeting.js
@@ -1,9 +1,26 @@
 var swiper = new Swiper(".meeting-slide", {
-  slidesPerView: 1,
   spaceBetween: 30,
-  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".meeting-medium", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
