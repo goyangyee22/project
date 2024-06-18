4c716c1a901135fd5b2ce65361c0dde4dfafaabf
seasukmin <4447447@naver.com>
2024-06-18 14:19:54 +0900

    update

--- resources/js/changePassword.js
+++ resources/js/changePassword.js
@@ -31,8 +31,11 @@ const firebaseConfig = {
 };
 
 // Firebase 앱 초기화
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+// 비밀번호 형식입니다.
+let pwEx = /^[0-9A-Za-z\d$@$!%*?&]{4,16}/g;
 
 // 로그인이 되어있지 않은 경우, 접근이 제한됩니다.
 const userInfo = sessionStorage.getItem("userInfo");
@@ -43,25 +46,42 @@ if (!userInfo) {
 
 const updateBtn = document.getElementById("updateBtn");
 updateBtn.addEventListener("click", async function () {
-  const form = document.forms["changePassword"];
-  const newPassword = form["newPassword"].value;
-  const newPasswordConfirm = form["newPasswordConfirm"].value;
+  const newPassword = document.querySelector("input[name='newPassword']").value;
+  const newPasswordConfirm = document.querySelector(
+    "input[name='newPasswordConfirm']"
+  ).value;
 
+  // 비밀번호 형식이 지켜지지 않을 경우 비밀번호를 변경할 수 없습니다.
+  if (!pwEx.test(newPassword)) {
+    alert(
+      "비밀번호 형식이 올바르지 않습니다. 4자-16자 사이의 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다."
+    );
+    return false;
+  }
+
+  // 새로운 비밀번호와 새로운 비밀번호 확인의 입력값이 일치하지 않으면 비밀번호를 변경할 수 없습니다.
   if (newPassword !== newPasswordConfirm) {
     alert("비밀번호가 일치하지 않습니다.");
     return false;
   }
 
-  const userId = "input[name='newPassword']";
+  // 새로운 비밀번호와 새로운 비밀번호 확인의 입력값이 일치하면 비밀번호가 변경 됩니다..
+  const userpw = document.querySelector("input[name='newPassword']").value;
 
   try {
-    const userDocRef = doc(dbService, "userInfo", userId);
+    const userDocRef = doc(db, "userInfo", userpw);
     await updateDoc(userDocRef, {
-      password: newPassword,
+      pw: newPassword,
     });
     alert("비밀번호가 성공적으로 변경되었습니다!");
+
+    // 세션 스토리지에 변경된 비밀번호를 업데이트 합니다.
+    sessionStorage.setItem("userInfo", userpw);
   } catch (error) {
     console.error("Error updating document: ", error);
     alert("비밀번호 변경 중 오류가 발생했습니다.");
   }
 });
+
+// 세션 스토리지에서 사용자 정보(pw) 업데이트
+// pw의 값이 변경되면 Firestore database에서 pw의 값 변경
