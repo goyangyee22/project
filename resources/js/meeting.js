6b3e9e3283740f67400e50faa497b1b9341a5099
bsybsy01 <bsybsy2109@gmail.com>
2024-06-18 12:42:06 +0900

    .

--- resources/js/meeting.js
+++ resources/js/meeting.js
@@ -17,10 +17,49 @@ var swiper = new Swiper(".meeting-slide", {
 
 var swiper = new Swiper(".meeting-medium", {
   slidesPerView: 3,
+  centeredSlides: true,
   spaceBetween: 30,
-  freeMode: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+  .querySelector(".prepend-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide([
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+    ]);
+  });
+document
+  .querySelector(".prepend-slide")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide(
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+    );
+  });
+document.querySelector(".append-slide").addEventListener("click", function (e) {
+  e.preventDefault();
+  swiper.appendSlide(
+    '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+  );
+});
+document
+  .querySelector(".append-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.appendSlide([
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+    ]);
+  });
