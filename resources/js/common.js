4f4ff6e780acc41ba395d88b9cc729497773e7e6
문희연 <yeon7513@nate.com>
2024-06-19 11:32:05 +0900

    common.js update

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,4 +1,4 @@
-// calendar
+// 달력 렌더링
 function calendarRendering(display, date) {
   const calendarEl = document.getElementById('calendar');
 
@@ -51,7 +51,7 @@ function calendarRendering(display, date) {
   calendar.render();
 }
 
-// 총 합계 계산 함수
+// 총 합계 계산 함수 (결제)
 function calculateAmount(display, personnel, amount) {
   // 방 옵션 선택
   const timeOption = document.querySelector('.pay-time-btn.selected');
@@ -70,4 +70,27 @@ function calculateAmount(display, personnel, amount) {
 
   display.innerHTML = totalCost.toLocaleString();
   amount.value = totalCost;
+}
+
+// 지도
+function mapRendering() {
+  var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+  var options = {
+    //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(36.328772, 127.423009), //지도의 중심좌표.
+    level: 3, //지도의 레벨(확대, 축소 정도)
+  };
+
+  var map = new kakao.maps.Map(container, options);
+
+  // 마커가 표시될 위치입니다
+  var markerPosition = new kakao.maps.LatLng(36.328772, 127.423009);
+
+  // 마커를 생성합니다
+  var marker = new kakao.maps.Marker({
+    position: markerPosition,
+  });
+
+  // 마커가 지도 위에 표시되도록 설정합니다
+  marker.setMap(map);
 }
