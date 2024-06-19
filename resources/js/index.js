a8de2a3a9ee362143bf241b2bcb0180dd4947b6e
seasukmin <4447447@naver.com>
2024-06-19 15:42:32 +0900

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -23,27 +23,22 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 function updateButtonState() {
-  const loginButton = document.querySelector(".spot-login a");
-  const joinButton = document.querySelector(".spot-join a");
-  const pageButton = document.querySelector(".spot-page a");
-
-  if (isLoggedIn()) {
-    loginButton.classList.add("disabled");
-    loginButton.classList.remove("abled");
-    // joinButton.classList.add("disabled");
-    // joinButton.classList.remove("abled");
-    // pageButton.classList.remove("disabled");
-    // pageButton.classList.add("abled");
-  } else {
-    loginButton.classList.add("abled");
-    loginButton.classList.remove("disabled");
-    // joinButton.classList.add("abled");
-    // joinButton.classList.remove("disabled");
-    // pageButton.classList.remove("abled");
-    // pageButton.classList.add("disabled");
+    const loginButton = document.querySelector(".spot-login a");
+    const joinButton = document.querySelector(".spot-join a");
+  
+    if (isLoggedIn()) {
+      loginButton.classList.add("disabled");
+      loginButton.classList.remove("abled");
+      joinButton.classList.add("disabled");
+      joinButton.classList.remove("abled");
+    } else {
+      loginButton.classList.add("abled");
+      loginButton.classList.remove("disabled");
+      joinButton.classList.add("abled");
+      joinButton.classList.remove("disabled");
+    }
   }
-}
-// DOMContentLoaded 이벤트가 발생할 때까지 기다립니다.
-document.addEventListener("DOMContentLoaded", function () {
-  updateButtonState();
-});
+  // DOMContentLoaded 이벤트가 발생할 때까지 기다립니다.
+  document.addEventListener("DOMContentLoaded", function () {
+    updateButtonState();
+  });
