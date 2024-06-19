d732601e6509668e23b53b45e66bbdc8ce14056e
seasukmin <4447447@naver.com>
2024-06-19 16:03:46 +0900

    Update postscript.js

--- resources/js/postscript.js
+++ resources/js/postscript.js
@@ -34,10 +34,11 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 // 로그인이 되어있지 않은 경우, 접근이 제한됩니다.
+// 로그인이 되어있지 않은 경우, 접근이 제한됩니다.
 const userInfo = sessionStorage.getItem("userInfo");
 if (!userInfo) {
-  alert("로그인이 되어있지 않은 경우 후기 작성이 제한됩니다.");
-  window.location.href = "../index.html";
+  alert("로그인을 해주세요.");
+  window.location.href = "./signIn.html";
 }
 
 // 게시글을 작성하는 함수입니다.
@@ -45,13 +46,14 @@ const updateBtn = document.getElementById("updateBtn");
 updateBtn.addEventListener("click", async (event) => {
   event.preventDefault();
   const postInfo = {
-    name: document.querySelector(sessionStorage.getItem("userInfo", name)).value,
+    name: document.querySelector(sessionStorage.getItem("userInfo", name))
+      .value,
     title: document.querySelector("input[name='title']").value,
     content: document.querySelector("input[name='content']").value,
   };
 
   const result = await addDatas("board", postInfo);
-  result ? getMembersHandleTrClick() : alert("작성에 실패했습니다.")
+  result ? window.location.reload() : alert("작성에 실패했습니다.");
 });
 
 // 게시글을 수정하는 함수입니다. (한 번에 한 개의 게시글씩 수정 가능)
