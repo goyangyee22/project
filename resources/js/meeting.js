382ad259f8c9d35cd1a836ec7223b8373b843348
bsybsy01 <bsybsy2109@gmail.com>
2024-06-24 16:20:44 +0900

    .

--- resources/js/meeting.js
+++ resources/js/meeting.js
@@ -65,4 +65,27 @@ function meeting(room) {
   });
 }
 
+// ---------------------------- 주소복사
+function urlCopy() {
+  var copyText = document.getElementById("text");
+  copyText.style.display = "block";
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(copyText.value).then(
+    function () {
+      var copyMessage = document.getElementById("copyMessage");
+      copyMessage.classList.add("show");
+      setTimeout(function () {
+        copyMessage.classList.remove("show");
+      }, 1000);
+    },
+    function (err) {
+      console.error("Failed to copy: ", err);
+    }
+  );
+
+  copyText.style.display = "none";
+}
+
 mapRendering();
