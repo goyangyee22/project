558a69cb1f012bac4677493729f1c86a7b3beebd
문희연 <yeon7513@nate.com>
2024-06-27 19:39:29 +0900

    header update

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,6 +1,6 @@
 // header spot menu rendering
-const userInfo = sessionStorage.getItem("userInfo");
-const spotMenu = document.querySelector(".spot");
+const userInfo = sessionStorage.getItem('userInfo');
+const spotMenu = document.querySelector('.spot');
 if (!userInfo) {
   // spot
   spotMenu.innerHTML = `
@@ -32,67 +32,67 @@ if (!userInfo) {
     </li>
 `;
 
-  document.getElementById("logout").addEventListener("click", function () {
-    if (confirm("정말 로그아웃 하시겠습니까?")) {
-      sessionStorage.removeItem("userInfo");
-      alert("로그아웃 되었습니다.");
+  document.getElementById('logout').addEventListener('click', function () {
+    if (confirm('정말 로그아웃 하시겠습니까?')) {
+      sessionStorage.removeItem('userInfo');
+      alert('로그아웃 되었습니다.');
       window.location.reload();
     }
   });
 }
 
 /* 햄버거 메뉴 */
-const hamMenu = document.querySelector(".ham-menu");
-const navEl = document.querySelector("nav");
-hamMenu.addEventListener("click", function () {
-  this.classList.toggle("show-menu");
-  if (this.classList.contains("show-menu")) {
-    navEl.style.display = "flex";
-    navEl.style.height = "4.5625rem";
-    navEl.firstElementChild.style.display = "flex";
+const hamMenu = document.querySelector('.ham-menu');
+const navEl = document.querySelector('nav');
+hamMenu.addEventListener('click', function () {
+  this.classList.toggle('show-menu');
+  if (this.classList.contains('show-menu')) {
+    navEl.style.display = 'flex';
+    navEl.style.height = '35px';
+    navEl.firstElementChild.style.display = 'flex';
   } else {
     navEl.style.height = 0;
-    navEl.firstElementChild.style.display = "none";
+    navEl.firstElementChild.style.display = 'none';
   }
 });
 
-window.addEventListener("resize", function () {
+window.addEventListener('resize', function () {
   if (window.innerWidth < 1024) {
     // 모바일 화면일 때
-    if (!hamMenu.classList.contains("show-menu")) {
+    if (!hamMenu.classList.contains('show-menu')) {
       navEl.style.height = 0;
-      navEl.firstElementChild.style.display = "none";
+      navEl.firstElementChild.style.display = 'none';
     }
   } else {
     // 데스크탑 화면일 때
-    hamMenu.classList.remove("show-menu");
-    navEl.style.display = "flex";
+    hamMenu.classList.remove('show-menu');
+    navEl.style.display = 'flex';
     navEl.style.height = 0;
-    navEl.firstElementChild.style.display = "flex";
+    navEl.firstElementChild.style.display = 'flex';
   }
 });
 
 // 페이지 로드 시 초기 상태 설정
 if (window.innerWidth < 1024) {
   navEl.style.height = 0;
-  navEl.firstElementChild.style.display = "none";
+  navEl.firstElementChild.style.display = 'none';
 } else {
-  navEl.style.display = "flex";
+  navEl.style.display = 'flex';
   navEl.style.height = 0;
-  navEl.firstElementChild.style.display = "flex";
+  navEl.firstElementChild.style.display = 'flex';
 }
 
 // 달력 렌더링
 function calendarRendering(display, date) {
-  const calendarEl = document.getElementById("calendar");
+  const calendarEl = document.getElementById('calendar');
 
   let calendar = new FullCalendar.Calendar(calendarEl, {
-    locale: "ko",
-    initialView: "dayGridMonth",
-    googleCalendarApiKey: "AIzaSyDjg0gbj5QCe_pmGqhJcl_510x5i-0C6-U",
+    locale: 'ko',
+    initialView: 'dayGridMonth',
+    googleCalendarApiKey: 'AIzaSyDjg0gbj5QCe_pmGqhJcl_510x5i-0C6-U',
     events: {
       googleCalendarId:
-        "07602c2e77c6a76840bc6eb4d079f9a4f653c0ba493d1a91469f9b276f437298@group.calendar.google.com",
+        '07602c2e77c6a76840bc6eb4d079f9a4f653c0ba493d1a91469f9b276f437298@group.calendar.google.com',
     },
     // editable: true,
     selectable: true,
@@ -110,7 +110,7 @@ function calendarRendering(display, date) {
       maxDate.setMonth(now.getMonth() + 2);
 
       if (start < now || start > maxDate) {
-        alert("최대 2개월 후만 예약이 가능합니다.");
+        alert('최대 2개월 후만 예약이 가능합니다.');
         calendar.usselect();
       }
     },
@@ -122,13 +122,13 @@ function calendarRendering(display, date) {
       return { html: e.date.getDate().toString() };
     },
     headerToolbar: {
-      start: "prev",
-      center: "title",
-      end: "next",
+      start: 'prev',
+      center: 'title',
+      end: 'next',
     },
     titleFormat: {
-      year: "numeric",
-      month: "2-digit",
+      year: 'numeric',
+      month: '2-digit',
     },
   });
 
@@ -138,7 +138,7 @@ function calendarRendering(display, date) {
 // 총 합계 계산 함수 (결제)
 function calculateAmount(display, personnel, amount) {
   // 방 옵션 선택
-  const timeOption = document.querySelector(".pay-time-btn.selected");
+  const timeOption = document.querySelector('.pay-time-btn.selected');
 
   if (!timeOption) {
     return;
@@ -158,7 +158,7 @@ function calculateAmount(display, personnel, amount) {
 
 // 지도
 function mapRendering() {
-  var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+  var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
   var options = {
     //지도를 생성할 때 필요한 기본 옵션
     center: new kakao.maps.LatLng(36.328772, 127.423009), //지도의 중심좌표.
