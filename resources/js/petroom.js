439188b1e6cf7430d8db086855b4216fa91ec918
Yanz <wjddmsy12345@gmail.com>
2024-06-27 11:29:24 +0900

    .

--- resources/js/petroom.js
+++ resources/js/petroom.js
@@ -10,19 +10,19 @@
 //   },
 // });
 
-var swiper = new Swiper('.detail-slide-thumb', {
+var swiper = new Swiper(".detail-slide-thumb", {
   loop: true,
   spaceBetween: 10,
   slidesPerView: 4,
   freeMode: true,
   watchSlidesProgress: true,
 });
-var swiper2 = new Swiper('.detail-slide', {
+var swiper2 = new Swiper(".detail-slide", {
   loop: true,
   spaceBetween: 10,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   thumbs: {
     swiper: swiper,
@@ -47,7 +47,7 @@ var swiper2 = new Swiper('.detail-slide', {
 
 function petroom(room) {
   console.log(room);
-  const gallery = document.querySelector('.room-body');
+  const gallery = document.querySelector(".room-body");
 
   let createImgStr = `
                       <div class="swiper petroom">
@@ -68,7 +68,7 @@ function petroom(room) {
                   `;
   gallery.innerHTML = createImgStr;
 
-  var swiper1 = new Swiper('.petroom', {
+  var swiper1 = new Swiper(".petroom", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
@@ -78,45 +78,53 @@ function petroom(room) {
     // },
     freeMode: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
     },
   });
 }
 // 주소 복사하기
 function urlCopy() {
-  var copyText = document.getElementById('text');
-  copyText.style.display = 'block';
+  var copyText = document.getElementById("text");
+  copyText.style.display = "block";
   copyText.select();
   copyText.setSelectionRange(0, 99999);
 
   navigator.clipboard.writeText(copyText.value).then(
     function () {
-      var copyMessage = document.getElementById('copyMessage');
-      copyMessage.classList.add('show');
+      var copyMessage = document.getElementById("copyMessage");
+      copyMessage.classList.add("show");
       setTimeout(function () {
-        copyMessage.classList.remove('show');
+        copyMessage.classList.remove("show");
       }, 1000);
     },
     function (err) {
-      console.error('Failed to copy: ', err);
+      console.error("Failed to copy: ", err);
     }
   );
 
-  copyText.style.display = 'none';
+  copyText.style.display = "none";
 }
 
 //메뉴버튼 숨기기'
 
-const btn = document.getElementById('menu-b');
-const menu = document.querySelector('.menu');
-btn.addEventListener('click', function () {
-  menu.classList.add('show-menus');
+const btn = document.getElementById("menu-b");
+const menu = document.querySelector(".menu");
+btn.addEventListener("click", function () {
+  menu.classList.add("show-menus");
 });
 
-const closeEl = document.querySelector('.close');
-closeEl.addEventListener('click', function () {
-  menu.classList.remove('show-menus');
+const closeEl = document.querySelector(".close");
+closeEl.addEventListener("click", function () {
+  menu.classList.remove("show-menus");
 });
+
+// 위로 가기 버튼
+function Topbtn() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
 
 mapRendering();
