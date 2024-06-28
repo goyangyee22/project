05d8efbc8eb085290fc264998a2985906c157d98
chajoonho <wnsgh882002@gmail.com>
2024-06-28 09:49:32 +0900

    .

--- resources/js/party.js
+++ resources/js/party.js
@@ -94,45 +94,6 @@ sectionMenus.forEach((link) => {
   });
 });
 
-function partyroom(room) {
-  console.log(room);
-  const gallery = document.querySelector(".party-body");
-
-  let createImgStr = `
-      <div class="swiper partyrooms">
-        <div class="swiper-wrapper" id="">
-          `;
-
-  for (let i = 1; i < 6; i++) {
-    createImgStr += `
-            <div class="swiper-slide roomimg1">
-              <img src="resources/images/partyimg/${room}${i}.jpg" alt="" />
-              </div>
-              `;
-  }
-  createImgStr += `
-            </div>
-            <div class="swiper-pagination"></div>
-            </div>
-            `;
-  gallery.innerHTML = createImgStr;
-
-  var swiper1 = new Swiper(".partyrooms", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-}
-
 function urlCopy() {
   var copyText = document.getElementById("text");
   copyText.style.display = "block"; // 임시로 텍스트 보이게 함
